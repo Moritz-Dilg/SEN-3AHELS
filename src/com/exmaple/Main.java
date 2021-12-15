a36b2a8f2c831aed1b3fee2dec5d6011c09474fa
Moritz.Dilg <moritz@dilg-online.de>
2021-12-15 15:42:51 +0100

    Initial commit: 15-12-2021

---
+++ src/com/exmaple/Main.java
@@ -0,0 +1,7 @@
+package com.exmaple;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/exmaple/recursion/Count_x.java
@@ -0,0 +1,21 @@
+package com.exmaple.recursion;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Scanner;
+
+public class Count_x {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter string: ");
+
+        String string = scanner.nextLine();
+        System.out.println(count_x(string));
+    }
+
+    static int count_x(@NotNull String s) {
+        if (s.length() == 1) return s.charAt(0)=='x' ? 1 : 0;
+
+        return count_x(s.substring(1)) + (s.charAt(0)=='x' ? 1 : 0);
+    }
+}
